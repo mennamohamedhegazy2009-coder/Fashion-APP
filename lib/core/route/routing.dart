d3f1bb512a070358bb80b9f5c4cfa31c88139706
Dalia Mohamed <mennamohamedhegazy2009@gmail.com>
2026-03-03 21:29:00 +0200

    Remove payment route from routing
    Commented out the payment route in the routing logic.

--- lib/core/route/routing.dart
+++ lib/core/route/routing.dart
@@ -42,8 +42,6 @@ abstract class AppRouter {
       case AppRoutes.checkout:
         return _materialRoute(const CheckoutScreen());
 
-      // case AppRoutes.payment:
-      //   return _materialRoute(const PaymentScreen());
 
       case AppRoutes.profile:
         return _materialRoute(const EditProfileScreen());
